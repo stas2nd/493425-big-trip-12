104323338b58bcaace6cce76354162aec41c9afb
Anastasiya Dobrotina <aydobrot@mts.ru>
2020-07-31 23:00:59 +0300

    1.2 Деление на отдельные компоненты

--- src/main.js
+++ src/main.js
@@ -1,6 +1,143 @@
 "use strict";
 
 const EVENT_COUNT = 3;
+const TAB_ARRAY = [
+  {
+    text: `Table`,
+    href: `#`,
+    state: `trip-tabs__btn--active`
+  },
+  {
+    text: `Stats`,
+    href: `#`
+  }
+];
+const FILTER_ARAY = [
+  {
+    value: `everything`,
+    text: `Everything`,
+    state: `checked`
+  },
+  {
+    value: `future`,
+    text: `Future`
+  },
+  {
+    value: `past`,
+    text: `Past`
+  }
+];
+const SORT_ITEM_ARRAY = [
+  {
+    interactive: false,
+    id: `day`,
+    text: `Day`
+  },
+  {
+    interactive: true,
+    id: `event`,
+    text: `Event`,
+    icon: false,
+    state: `checked`
+  },
+  {
+    interactive: true,
+    id: `time`,
+    text: `Time`,
+    icon: true
+  },
+  {
+    interactive: true,
+    id: `price`,
+    text: `Price`,
+    icon: true
+  },
+  {
+    interactive: false,
+    id: `offers`,
+    text: `Offers`
+  }
+];
+const EVENT_TRANSPORT_ARRAY = [
+  {
+    value: `taxi`,
+    text: `Taxi`
+  },
+  {
+    value: `bus`,
+    text: `Bus`
+  },
+  {
+    value: `train`,
+    text: `Train`
+  },
+  {
+    value: `ship`,
+    text: `Ship`
+  },
+  {
+    value: `transport`,
+    text: `Transport`
+  },
+  {
+    value: `drive`,
+    text: `Drive`
+  },
+  {
+    value: `flight`,
+    text: `Flight`,
+    state: `checked`
+  },
+];
+const EVENT_ACTIVITY_ARRAY = [
+  {
+    value: `check-in`,
+    text: `Check-in`
+  },
+  {
+    value: `sightseeing`,
+    text: `Sightseeing`
+  },
+  {
+    value: `restaurant`,
+    text: `Restaurant`
+  },
+];
+const OFFER_ARAY = [
+  {
+    id: `luggage`,
+    text: `Add luggage`,
+    price: 30,
+    state: `checked`
+  },
+  {
+    id: `comfort`,
+    text: `Switch to comfort class`,
+    price: 100,
+    state: `checked`
+  },
+  {
+    id: `meal`,
+    text: `Add meal`,
+    price: 15
+  },
+  {
+    id: `seats`,
+    text: `Choose seats`,
+    price: 5
+  },
+  {
+    id: `train`,
+    text: `Travel by train`,
+    price: 40
+  }
+];
+
+const makeTemplateFromArray = (array, func) => {
+  return array?.reduce((accumulator, currentValue) => {
+    return accumulator + func(...Object.values(currentValue))
+  }, ``) || ``;
+}
 
 const createHeadInfoTemplate = () => {
   return (
@@ -26,71 +163,64 @@ const createHeadPriceTemplate = () => {
   );
 };
 
-const createMenuTemplate = () => {
+const createMenuTabTemplate = (text, href, state = ``) => {
+  return (
+    `<a class="trip-tabs__btn ${state}" href="${href}">${text}</a>`
+  );
+};
+
+const createMenuTemplate = (links) => {
+  links = makeTemplateFromArray(links, createMenuTabTemplate);
   return (
     `<h2 class="visually-hidden">Switch trip view</h2>
     <nav class="trip-controls__trip-tabs  trip-tabs">
-      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-      <a class="trip-tabs__btn" href="#">Stats</a>
+      ${links}
     </nav>`
   );
 };
 
-const createFilterTemplate = () => {
+const createFilterItemTemplate = (value, text, state = '') => {
+  return (
+    `<div class="trip-filters__filter">
+      <input id="filter-${value}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${value}" ${state}>
+      <label class="trip-filters__filter-label" for="filter-${value}">${text}</label>
+    </div>`
+  );
+}
+
+const createFilterTemplate = (filters) => {
+  filters = makeTemplateFromArray(filters, createFilterItemTemplate);
   return (
     `<h2 class="visually-hidden">Filter events</h2>
     <form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
-
+      ${filters}
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`
   );
 };
 
-const createSortingTemplate = () => {
+const createSortingItemTemplate = (interactive, id, text, icon = false, state =``) => {
+  const imgIcon = `<svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+  </svg>`;
+  return (
+    interactive ?
+    `<div class="trip-sort__item  trip-sort__item--${id}">
+      <input id="sort-${id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${id}" ${state}>
+      <label class="trip-sort__btn" for="sort-${id}">
+        ${text}
+        ${icon ? imgIcon : ''}
+      </label>
+    </div>`
+    : `<span class="trip-sort__item  trip-sort__item--${id}">${text}</span>`
+  );
+}
+
+const createSortingTemplate = (sortItems) => {
+  sortItems = makeTemplateFromArray(sortItems, createSortingItemTemplate);
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <span class="trip-sort__item  trip-sort__item--day">Day</span>
-
-      <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn" for="sort-time">
-          Time
-          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-          </svg>
-        </label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
-          Price
-          <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-          </svg>
-        </label>
-      </div>
-
-      <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+      ${sortItems}
     </form>`
   );
 };
@@ -115,7 +245,19 @@ const createDayTemplate = () => {
   );
 };
 
-const createEditingEventTemplate = () => {
+const createEditingEventOptionTemplate = (value, text, state = '') => {
+  return (
+    `<div class="event__type-item">
+      <input id="event-type-${value}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${value}">
+      <label class="event__type-label  event__type-label--${value}" for="event-type-${value}-1">${text}</label>
+    </div>`
+  );
+}
+
+const createEditingEventTemplate = (transportOpts, activityOpts) => {
+  transportOpts = makeTemplateFromArray(transportOpts, createEditingEventOptionTemplate);
+  activityOpts = makeTemplateFromArray(activityOpts, createEditingEventOptionTemplate);
+
   return (
     `<form class="trip-events__item  event  event--edit" action="#" method="post">
         <header class="event__header">
@@ -129,60 +271,12 @@ const createEditingEventTemplate = () => {
             <div class="event__type-list">
               <fieldset class="event__type-group">
                 <legend class="visually-hidden">Transfer</legend>
-
-                <div class="event__type-item">
-                  <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-                  <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-                  <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-                  <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-                  <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-transport-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="transport">
-                  <label class="event__type-label  event__type-label--transport" for="event-type-transport-1">Transport</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-                  <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-                  <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-                </div>
+                ${transportOpts}
               </fieldset>
 
               <fieldset class="event__type-group">
                 <legend class="visually-hidden">Activity</legend>
-
-                <div class="event__type-item">
-                  <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-                  <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-                  <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-                </div>
-
-                <div class="event__type-item">
-                  <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-                  <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-                </div>
+                ${activityOpts}
               </fieldset>
             </div>
           </div>
@@ -234,55 +328,27 @@ const createEditingEventDetailsTemplate = () => {
   );
 };
 
-const createEditingEventOffersTemplate = () => {
+const createEditingEventOfferItemTemplate = (id, text, price, state = '') => {
+  return (
+    `<div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${id}-1" type="checkbox" name="event-offer-${id}" ${state}>
+      <label class="event__offer-label" for="event-offer-${id}-1">
+        <span class="event__offer-title">${text}</span>
+        &plus;
+        &euro;&nbsp;<span class="event__offer-price">${price}</span>
+      </label>
+    </div>`
+  );
+}
+
+const createEditingEventOffersTemplate = (offers) => {
+  offers = makeTemplateFromArray(offers, createEditingEventOfferItemTemplate);
+
   return (
     `<section class="event__section  event__section--offers">
       <h3 class="event__section-title  event__section-title--offers">Offers</h3>
       <div class="event__available-offers">
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-          <label class="event__offer-label" for="event-offer-luggage-1">
-            <span class="event__offer-title">Add luggage</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">30</span>
-          </label>
-        </div>
-
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-          <label class="event__offer-label" for="event-offer-comfort-1">
-            <span class="event__offer-title">Switch to comfort class</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">100</span>
-          </label>
-        </div>
-
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-          <label class="event__offer-label" for="event-offer-meal-1">
-            <span class="event__offer-title">Add meal</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">15</span>
-          </label>
-        </div>
-
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-          <label class="event__offer-label" for="event-offer-seats-1">
-            <span class="event__offer-title">Choose seats</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">5</span>
-          </label>
-        </div>
-
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-          <label class="event__offer-label" for="event-offer-train-1">
-            <span class="event__offer-title">Travel by train</span>
-            &plus;
-            &euro;&nbsp;<span class="event__offer-price">40</span>
-          </label>
-        </div>
+        ${offers}
       </div>
     </section>`
   );
@@ -361,17 +427,17 @@ render(siteHeaderInfoElement, createHeadRouteTemplate(), `beforeend`);
 render(siteHeaderInfoElement, createHeadPriceTemplate(), `beforeend`);
 
 const siteHeaderControlsElement = siteHeaderMainElement.querySelector(`.trip-controls`);
-render(siteHeaderControlsElement, createMenuTemplate(), `beforeend`);
-render(siteHeaderControlsElement, createFilterTemplate(), `beforeend`);
+render(siteHeaderControlsElement, createMenuTemplate(TAB_ARRAY), `beforeend`);
+render(siteHeaderControlsElement, createFilterTemplate(FILTER_ARAY), `beforeend`);
 
 const siteContentElement = siteMainElement.querySelector(`.trip-events`);
-render(siteContentElement, createSortingTemplate(), `beforeend`);
+render(siteContentElement, createSortingTemplate(SORT_ITEM_ARRAY), `beforeend`);
 
-render(siteContentElement, createEditingEventTemplate(), `beforeend`);
+render(siteContentElement, createEditingEventTemplate(EVENT_TRANSPORT_ARRAY, EVENT_ACTIVITY_ARRAY), `beforeend`);
 const siteEditingFormElement = siteContentElement.querySelector(`.event--edit`);
 render(siteEditingFormElement, createEditingEventDetailsTemplate(), `beforeend`);
 const siteEditingFormDetailsElement = siteEditingFormElement.querySelector(`.event__details`);
-render(siteEditingFormDetailsElement, createEditingEventOffersTemplate(), `beforeend`);
+render(siteEditingFormDetailsElement, createEditingEventOffersTemplate(OFFER_ARAY), `beforeend`);
 render(siteEditingFormDetailsElement, createEditingEventDestinationTemplate(), `beforeend`);
 
 render(siteContentElement, createListDaysTemplate(), `beforeend`);
